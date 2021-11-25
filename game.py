7b9024721d841b7c89171a87becba17f1c9b69b3
XeniusDZ <76628837+XeniusDZ@users.noreply.github.com>
2021-11-25 19:54:57 +0300

    game

--- game.py
+++ game.py
@@ -1,10 +1,8 @@
 import pygame
-from pygame.constants import QUIT
+from pygame.constants import K_DOWN, K_LEFT, QUIT, K_d, K_q, K_s
 import pyscroll
 import pytmx
 from player import Player
-
-
 
 class Start:
     def __init__(self):
@@ -21,14 +19,84 @@ class Start:
         position = tmx.get_object_by_name("spawn")
         self.player = Player(position.x,position.y)
 
+        #collisions
+        self.walls = []
+
+        for object in tmx.objects:
+            if object.type == "collision":
+                self.walls.append(pygame.Rect(object.x, object.y, object.width, object.height))
+            
+
         #draw layers
-        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=3)
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=6)
         self.group.add(self.player)
+        
+
+
+    def clavish(self):
+        pressed = pygame.key.get_pressed()
+        if (pressed[pygame.K_z] or pressed[pygame.K_UP]) and (pressed[pygame.K_d] or pressed[pygame.K_RIGHT]):
+            self.player.orizental_right_up()
+            self.player.animation(self.player.directions["up"],self.player.count)
+            self.player.count += 1
+            if self.player.count > 2:
+                self.player.count = 0
+        elif (pressed[pygame.K_z] or pressed[pygame.K_UP]) and (pressed[pygame.K_q] or pressed[pygame.K_LEFT]):
+            self.player.orizental_left_up()
+            self.player.animation(self.player.directions["up"],self.player.count)
+            self.player.count += 1
+            if self.player.count > 2:
+                self.player.count = 0
+        elif (pressed[pygame.K_s] or pressed[pygame.K_DOWN]) and (pressed[pygame.K_d] or pressed[pygame.K_RIGHT]):
+            self.player.orizental_right_down()
+            self.player.animation(self.player.directions["down"],self.player.count)
+            self.player.count += 1
+            if self.player.count > 2:
+                self.player.count = 0
+        elif (pressed[pygame.K_s] or pressed[pygame.K_DOWN]) and (pressed[pygame.K_q] or pressed[pygame.K_LEFT]):
+            self.player.orizental_left_down()
+            self.player.animation(self.player.directions["down"],self.player.count)
+            self.player.count += 1
+            if self.player.count > 2:
+                self.player.count = 0
+        elif pressed[pygame.K_z] or pressed[pygame.K_UP]:
+            self.player.up()
+            self.player.animation(self.player.directions["up"],self.player.count)
+            self.player.count += 1
+            if self.player.count > 2:
+                self.player.count = 0
+        elif pressed[pygame.K_s] or pressed[pygame.K_DOWN]:
+            self.player.down()
+            self.player.animation(self.player.directions["down"],self.player.count)
+            self.player.count += 1
+            if self.player.count > 2:
+                self.player.count = 0
+        elif pressed[pygame.K_d] or pressed[pygame.K_RIGHT]:
+            self.player.right()
+            self.player.animation(self.player.directions["right"],self.player.count)
+            self.player.count += 1
+            if self.player.count > 2:
+                self.player.count = 0
+        elif pressed[pygame.K_q] or pressed[pygame.K_LEFT]:  
+            self.player.left()
+            self.player.animation(self.player.directions["left"],self.player.count)
+            self.player.count += 1
+            if self.player.count > 2:
+                self.player.count = 0
+
+   
     def run(self):
-                #Loop for running
+        #fps
+        clock = pygame.time.Clock()
+
+        #Loop for running
         running = True
 
+
         while running:
+            
+            self.clavish()
+
             self.group.update()
             self.group.center(self.player.rect)
             self.group.draw(self.screen)
@@ -37,3 +105,5 @@ class Start:
                 if event.type == pygame.QUIT:
                     running = False
                     pygame.quit()
+            #modify to 60fps
+            clock.tick(60)
--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+from typing import Counter
 import pygame
 
 class Player(pygame.sprite.Sprite):
@@ -10,6 +11,55 @@ class Player(pygame.sprite.Sprite):
         self.x = x
         self.y = y
         self.position = [x,y]
+        self.animations = [[self.get_image(0,0),self.get_image(32,0),self.get_image(64,0),self.get_image(96,0)],
+        [self.get_image(0,32),self.get_image(32,32),self.get_image(64,32),self.get_image(96,32)],
+        [self.get_image(0,64),self.get_image(32,64),self.get_image(64,64),self.get_image(96,64)],
+        [self.get_image(0,96),self.get_image(32,96),self.get_image(64,96),self.get_image(96,96)]]
+        self.directions = {
+            "up" : 3,
+            "left" : 1,
+            "right" : 2,
+            "down" : 0
+        }
+        self.count = 0
+        self.feet = pygame.Rect(0,0, self.rect.width*0.5, 12)
+        self.past_position = self.position.copy()
+
+
+        
+    #change animation
+    def animation(self,direction,count):
+        self.image = self.animations[direction][count]
+        self.image.set_colorkey((0,0,0))
+
+        
+        
+
+
+    #change position
+    def orizental_right_up(self):
+        self.position[0] += 2
+        self.position[1] -= 2
+    def orizental_left_up(self):
+        self.position[0] -= 2
+        self.position[1] -= 2
+    def orizental_right_down(self):
+        self.position[0] += 2
+        self.position[1] += 2
+    def orizental_left_down(self):
+        self.position[0] -= 2
+        self.position[1] += 2
+
+    def right(self):
+        self.position[0] += 2
+    def left(self):
+        self.position[0] -= 2
+    def down(self):
+        self.position[1] += 2
+    def up(self):
+        self.position[1] -= 2
+    
+
 
     def update(self):
         self.rect.topleft = self.position
