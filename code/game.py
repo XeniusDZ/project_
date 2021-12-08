a1e4bcde996d84fdb16fdc45001c597a7dcf1c0e
XeniusDZ <76628837+XeniusDZ@users.noreply.github.com>
2021-12-08 10:41:03 +0300

    game

--- code/game.py
+++ code/game.py
@@ -13,7 +13,7 @@ class Start:
 
 
         #create game window
-        self.screen = pygame.display.set_mode((1080,720))
+        self.screen = pygame.display.set_mode((1920,1080))
         pygame.display.set_caption("Terros")
 
 
